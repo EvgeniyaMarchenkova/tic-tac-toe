7ea442bad1b5e2313468e2079bd7d8acd057c972
Evgeniya Marchenkova <evgeniova@gmail.com>
2017-02-04 15:41:47 +0300

    tic-tac-toe: all bags fixed

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -25,64 +25,60 @@ class TicTacToe {
             }
             else {
                 this.symbol = 'x';
-            };
-        };
+            }
+        }
+        this.getWinner();
     }
 
     isFinished() {
         if (this.winner) {
              return true;
-        };
-        for (var i = 0; i < this.fieldArr.length; i++) {
-            for (var j = 0; j < this.fieldArr[i].length; j++) {
-                if ((!this.fieldArr[i][j]) ) {
-                    return false;
-                };
-            };
-        };
-        return true;
+        }
+        return this.noMoreTurns();
     };
 
     getWinner() {
-        if ((this.fieldArr[0][0] == this.fieldArr[1][1]) && (this.fieldArr[1][1] == this.fieldArr[2][2])) {
+        if ((this.fieldArr[0][0] == this.fieldArr[1][1]) && (this.fieldArr[1][1] == this.fieldArr[2][2]) && (this.fieldArr[0][0])) {
             this.winner = this.fieldArr[0][0];
-            return;
+            return this.winner;
         }
-        else if ((this.fieldArr[2][0] == this.fieldArr[1][1]) && (this.fieldArr[1][1] == this.fieldArr[0][2])) {
+
+        if ((this.fieldArr[2][0] == this.fieldArr[1][1]) && (this.fieldArr[1][1] == this.fieldArr[0][2]) && (this.fieldArr[2][0])) {
             this.winner = this.fieldArr[2][0];
-            return;
+            return this.winner;
         }
-        ;
+
         for (var i = 0; i < this.fieldArr.length; i++) {
-            for (var j = 0; j < this.fieldArr.length[0]; j++) {
-                if ((this.fieldArr[i][0] == this.fieldArr[i][1]) && (this.fieldArr[i][1] == this.fieldArr[i][2])) {
+            for (var j = 0; j < this.fieldArr[i].length; j++) {
+                if ((this.fieldArr[i][0] == this.fieldArr[i][1]) && (this.fieldArr[i][1] == this.fieldArr[i][2]) && (this.fieldArr[i][0])) {
                     this.winner = this.fieldArr[i][0];
-                    return;
-                };
-                if ((this.fieldArr[0][j] == this.fieldArr[1][j]) && (this.fieldArr[1][j] == this.fieldArr[2][j])) {
+                    return this.winner;
+                }
+                if ((this.fieldArr[0][j] == this.fieldArr[1][j]) && (this.fieldArr[1][j] == this.fieldArr[2][j]) && (this.fieldArr[0][j])) {
                     this.winner = this.fieldArr[0][j];
-                    return;
-                };
-            };
-        };
+                    return this.winner;
+                }
+            }
+        }
+        return null;
     };
 
     noMoreTurns() {
             for (var i = 0; i < this.fieldArr.length; i++) {
                 for (var j = 0; j < this.fieldArr[i].length; j++) {
-                    if ((!this.fieldArr[i][j]) ) {
+                    if (!this.fieldArr[i][j]) {
                         return false;
-                    };
-                };
-            };
+                    }
+                }
+            }
             return true;
     }
 
     isDraw() {
-        var responcenoMoreTurns = this.noMoreTurns();
-        if ((!this.winner) && responcenoMoreTurns) {
+        var responseNoMoreTurns = this.noMoreTurns();
+        if (!this.winner && responseNoMoreTurns) {
             return true;
-        };
+        }
         return false;
     }
 
